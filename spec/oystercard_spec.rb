bb8d82b28d6c24405eca9064e5dd12ad2cff1dec
sjtinsley <simonjtinsley@gmail.com>
2022-04-25 14:57:54 +0100

    Add topup functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,4 +3,9 @@ describe Oystercard do
   it 'tests that a new card has a balance of 0' do
     expect(subject.balance).to eq 0
   end
+
+  it 'user can add money to Oystercard and it goes onto the balance' do
+    subject.top_up(5)
+    expect(subject.balance).to eq 5
+  end
 end
